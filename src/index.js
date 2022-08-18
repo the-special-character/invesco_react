051ed7c311bc6a7e405614a78ef06eb745ccdd54
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-08-18 21:20:42 +0530

    started react setup

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+// hello
+// var app = require("./app");
+import { a, b, abc } from './app'
+
+class Animal{
+    makeSound() {
+        console.log("bow woe");
+    }
+}
+
+const animal = new Animal();
+
+console.log(animal.makeSound());
+
+
+console.log(a + b);
+
+console.log(abc());
