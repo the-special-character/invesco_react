1002c0d6ba2e176e17564d9e570798d2fa94f5fb
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-08-22 18:31:01 +0530

    setup project

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ export const a = 1;
 export const b = 2;
 
 export function abc() {
-    return "hello"
+  return 'hello';
 }
 
 // module.exports = {
--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-
 // hello
 // var app = require("./app");
-import { a, b, abc } from './app'
+import { a, b, abc } from './app';
 
-class Animal{
-    makeSound() {
-        console.log("bow woe");
-    }
+class Animal {
+  static makeSound() {
+    console.log('bow woe');
+  }
+
+  #xValue = 0;
+
+  get #x() {
+    return this.#xValue;
+  }
+
+  set #x(value) {
+    this.#xValue = value;
+  }
+
+  #clicked() {
+    this.#x++;
+  }
 }
 
 const animal = new Animal();
 
 console.log(animal.makeSound());
-
 
 console.log(a + b);
 
