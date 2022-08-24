bb5c91ef1965901604e1dde21cbccc80c6cb0100
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-08-24 19:36:24 +0530

    break 2

--- src/Todo/index.jsx
+++ src/Todo/index.jsx
@@ -5,27 +5,60 @@ export default class Todo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todoText: '',
+      //   todoText: '',
+      todoList: [],
     };
   }
 
-  //   changeTodoText = () => {};
+  //   changeTodoText = event => {
+  //     this.setState({
+  //       todoText: event.target.value,
+  //     });
+  //   };
+
+  addTodo = event => {
+    event.preventDefault();
+
+    // document.addEventListener("copy", () => {
+
+    // })
+
+    this.setState(
+      ({ todoList }) => ({
+        todoList: [...todoList, this.todoTextRef.value],
+      }),
+      () => {
+        // console.log(this.state.todoList.length);
+        this.todoTextRef.value = '';
+      },
+    );
+  };
 
   render() {
-    const { todoText } = this.state;
+    console.log('render');
+
+    const { todoList } = this.state;
 
     return (
       <div className="container">
         <h1>Todo List</h1>
-        <form>
+        <form onSubmit={this.addTodo}>
           <input
+            ref={ref => {
+              this.todoTextRef = ref;
+            }}
             type="text"
             placeholder="Write your todo here..."
-            value={todoText}
+            // value={todoText}
             // onChange={this.changeTodoText}
           />
           <button type="submit">Add Todo</button>
         </form>
+        <div>
+          {todoList.map(item => (
+            <p>{item}</p>
+          ))}
+        </div>
       </div>
     );
   }
