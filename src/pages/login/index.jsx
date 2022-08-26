c7510ad30f58bfabbeab9b7bc658c9b9a07008e8
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-08-26 20:38:53 +0530

    basic formik completed

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,107 +1,142 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
+import { Formik, Form } from 'formik';
 import React from 'react';
-import { ThemeContext } from '../../context/themeContext';
+
+const wait = time =>
+  new Promise(resolve => {
+    setTimeout(resolve, time);
+  });
 
 function Login() {
-  console.log('Login render');
   return (
-    <>
-      <ThemeContext.Consumer>
-        {value => {
-          console.log('inner login theme');
-          return (
-            <div className="flex flex-col">
-              <h1>{`Current Theme is ${value.theme}`}</h1>
-              <button
-                type="button"
-                onClick={value.changeTheme}
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+        rememberMe: false,
+      }}
+      onSubmit={async (values, actions) => {
+        await wait(3000);
+        console.log(values);
+        actions.resetForm();
+      }}
+      validate={values => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required...';
+        }
+        if (!values.password) {
+          errors.password = 'Required...';
+        }
+        return errors;
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        isValid,
+        dirty,
+        isSubmitting,
+        handleChange,
+        handleBlur,
+      }) => (
+        <Form className="mt-8 space-y-6">
+          <input
+            type="hidden"
+            name="remember"
+            defaultValue="true"
+          />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label
+                htmlFor="email-address"
+                className="sr-only"
               >
-                Change Theme
-              </button>
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {touched.email && !!errors.email && (
+                <p className="font-light text-sm text-red-600">
+                  {errors.email}
+                </p>
+              )}
             </div>
-          );
-        }}
-      </ThemeContext.Consumer>
-      <form
-        className="mt-8 space-y-6"
-        action="#"
-        method="POST"
-      >
-        <input
-          type="hidden"
-          name="remember"
-          defaultValue="true"
-        />
-        <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {touched.password && !!errors.password && (
+                <p className="font-light text-sm text-red-600">
+                  {errors.password}
+                </p>
+              )}
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                id="remember-me"
+                name="rememberMe"
+                type="checkbox"
+                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                checked={values.rememberMe}
+                onChange={handleChange}
+              />
+              <label
+                htmlFor="remember-me"
+                className="ml-2 block text-sm text-gray-900"
+              >
+                Remember me
+              </label>
+            </div>
+
+            <div className="text-sm">
+              <a
+                href="#forgotPassword"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Forgot your password?
+              </a>
+            </div>
+          </div>
+
           <div>
-            <label
-              htmlFor="email-address"
-              className="sr-only"
+            <button
+              disabled={isSubmitting || !(dirty && isValid)}
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:bg-slate-400 disabled:cursor-not-allowed"
             >
-              Email address
-            </label>
-            <input
-              id="email-address"
-              name="email"
-              type="email"
-              autoComplete="email"
-              required
-              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Email address"
-            />
+              Sign in
+            </button>
           </div>
-          <div>
-            <label htmlFor="password" className="sr-only">
-              Password
-            </label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              autoComplete="current-password"
-              required
-              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Password"
-            />
-          </div>
-        </div>
-
-        <div className="flex items-center justify-between">
-          <div className="flex items-center">
-            <input
-              id="remember-me"
-              name="remember-me"
-              type="checkbox"
-              className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-            />
-            <label
-              htmlFor="remember-me"
-              className="ml-2 block text-sm text-gray-900"
-            >
-              Remember me
-            </label>
-          </div>
-
-          <div className="text-sm">
-            <a
-              href="#forgotPassword"
-              className="font-medium text-indigo-600 hover:text-indigo-500"
-            >
-              Forgot your password?
-            </a>
-          </div>
-        </div>
-
-        <div>
-          <button
-            type="submit"
-            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Sign in
-          </button>
-        </div>
-      </form>
-    </>
+        </Form>
+      )}
+    </Formik>
   );
 }
 
